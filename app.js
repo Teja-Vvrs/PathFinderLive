85fab94f3ec6919985c0108268a11b2b80c11a9d
Teja-Vvrs <tejav2527@gmail.com>
2025-04-02 11:28:22 +0530

    FinalCommit

--- app.js
+++ app.js
@@ -16,34 +16,33 @@ app.use(helmet({
             "script-src": ["'self'", "https://cdnjs.cloudflare.com", "https://cdn.socket.io"],
             "style-src": ["'self'", "https://cdnjs.cloudflare.com", "'unsafe-inline'"],
             "connect-src": ["'self'", "https://cdn.socket.io"],
-            "img-src": ["'self'", "data:", "https://*.tile.openstreetmap.org", "https://cdnjs.cloudflare.com", "http://localhost:3000"], // Allow local images
+            "img-src": ["'self'", "data:", "https://*.tile.openstreetmap.org", "https://cdnjs.cloudflare.com", "http://localhost:3000"],
             "font-src": ["'self'", "https://cdnjs.cloudflare.com"],
         },
     },
 }));
 
-
-
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", function(socket){
-    socket.on("send-location", function(data){
-        io.emit("receive-location", { id: socket.id, ...data });
+io.on("connection", function(socket) {
+    console.log(`New client connected: ${socket.id}`); 
+
+    socket.on("send-location", function(data) {
+        console.log(`Location received from ${socket.id}:`, data); 
+        io.emit("receive-location", { id: socket.id, ...data }); 
     });
 
-    socket.on("disconnect", function(){
+    socket.on("disconnect", function() {
+        console.log(`Client disconnected: ${socket.id}`);
         io.emit("user-disconnected", socket.id);
     });
 });
 
-
-app.get("/", function (req, res){
+app.get("/", function(req, res) {
     res.render("index");
 });
-
-
 
 server.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,16 @@
 const socket = io();
 
+console.log("Socket.IO client initialized, ID:", socket.id); // Log client socket ID
+
 if (navigator.geolocation) {
     navigator.geolocation.watchPosition(
         (position) => {
             const { latitude, longitude } = position.coords;
+            console.log(`Sending location from ${socket.id}:`, { latitude, longitude });
             socket.emit("send-location", { latitude, longitude });
         },
         (error) => {
-            console.log(error);
+            console.error("Geolocation error:", error);
         },
         {
             enableHighAccuracy: true,
@@ -17,30 +20,44 @@ if (navigator.geolocation) {
     );
 }
 
-const map = L.map("map").setView([0, 0], 16);
+const map = L.map("map").setView([0, 0], 2);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "© OpenStreetMap contributors",
 }).addTo(map);
-
-
-L.Icon.Default.prototype.options.iconUrl = "/images/marker-icon.png";
-L.Icon.Default.prototype.options.iconRetinaUrl = "/images/marker-icon-2x.png";
-L.Icon.Default.prototype.options.shadowUrl = "/images/marker-shadow.png";
 
 const markers = {};
 
 socket.on("receive-location", (data) => {
     const { id, latitude, longitude } = data;
-    map.setView([latitude, longitude], 16);
-    
+    console.log(`Received location for ${id}:`, { latitude, longitude });
+
+    const customIcon = L.icon({
+        iconUrl: "/images/marker-icon.png",
+        iconRetinaUrl: "/images/marker-icon-2x.png",
+        shadowUrl: "/images/marker-shadow.png",
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
     if (markers[id]) {
         markers[id].setLatLng([latitude, longitude]);
     } else {
-        markers[id] = L.marker([latitude, longitude]).addTo(map);
+        markers[id] = L.marker([latitude, longitude], { icon: customIcon }).addTo(map);
+        if (Object.keys(markers).length === 1) {
+            map.setView([latitude, longitude], 16);
+        }
+    }
+
+    const bounds = Object.values(markers).map(marker => marker.getLatLng());
+    if (bounds.length > 1) {
+        map.fitBounds(bounds);
     }
 });
 
 socket.on("user-disconnected", (id) => {
+    console.log(`User disconnected: ${id}`);
     if (markers[id]) {
         map.removeLayer(markers[id]);
         delete markers[id];
